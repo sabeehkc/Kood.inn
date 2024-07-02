7c261a256e48a7c380cd7bfca34eb304ed7ce223
sabeehkc <wg457129@gmail.com>
2024-07-02 19:31:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 60)
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    let menu = document.querySelector('#menu-icon');
+    let navBar = document.querySelector('.navbar');
+    let menuItems = document.querySelectorAll('.navbar li a');
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navBar.classList.toggle('open');
+    };
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('bx-x');
+            navBar.classList.remove('open');
+        });
+    });
+});
