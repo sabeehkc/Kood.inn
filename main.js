0106bf0f96329af2407e821e32f29121c6c4dafd
sabeehkc <wg457129@gmail.com>
2024-08-01 21:17:32 +0530

    kashmir page almost done

--- main.js
+++ main.js
@@ -22,7 +22,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-function helo(){
-    console.log("Hello");
-}
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 550) { 
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+});
